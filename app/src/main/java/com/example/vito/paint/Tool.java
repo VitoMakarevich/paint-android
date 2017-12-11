e3d0e6ba12aaf645b030e935734624d088aa3a8b
v.makarevich@hackspace.by <vitaliy.makarevich.work@gmail.com>
2017-12-11 11:57:46 +0300

    add all

---
+++ app/src/main/java/com/example/vito/paint/Tool.java
@@ -0,0 +1,13 @@
+package com.example.vito.paint;
+
+/**
+ * Created by vito on 06/12/17.
+ */
+
+public class Tool {
+    public static final int BRUSH     = 1;
+    public static final int RECTANGLE = 2;
+    public static final int OVAL      = 3;
+    public static final int TEXT      = 4;
+    public static final int MOVE      = 5;
+}
